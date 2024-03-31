6202b5900ed25829f48343d42ca924fd0b283e57
Luis Marquez <luis.marquez@e2ycommerce.com>
2024-03-31 21:31:15 +0200

    10 ciclos de vida

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,30 @@ export default {
       currentComponent: "HelloWorld",
     };
   },
+  beforeCreate() {
+    console.log("beforeCreate", this.$data, this.$el);
+  },
+  created() {
+    console.log("created", this.$data, this.$el);
+  },
+  beforeMount() {
+    console.log("beforeMount", this.$data, this.$el);
+  },
+  mounted() {
+    console.log("mounted", this.$data, this.$el);
+  },
+  beforeUpdate() {
+    console.log("beforeUpdate", this.$data, this.$el);
+  },
+  updated() {
+    console.log("updated", this.$data, this.$el);
+  },
+  beforeUnmount() {
+    console.log("beforeDestroy", this, this.data$);
+  },
+  unmounted() {
+    console.log("destroyed", this, this.data$);
+  },
 };
 </script>
 
